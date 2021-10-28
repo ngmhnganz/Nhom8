2dab6f0242286a32ff5a5789699d0d41de8bcf0a
Ngan Nguyen <ngannm19411c@st.uel.edu.vn>
2021-10-28 15:17:42 +0700

    thêm setOnClickListener cho txtSeemore

--- app/src/main/java/com/mcommerce/adapter/CategoryAdapter.java
+++ app/src/main/java/com/mcommerce/adapter/CategoryAdapter.java
@@ -1,6 +1,7 @@
 package com.mcommerce.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mcommerce.model.Category;
+import com.mcommerce.nhom8.ListProductActivity;
 import com.mcommerce.nhom8.R;
 
 import java.util.List;
@@ -57,6 +59,15 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.Catego
         ProductAdapter productAdapter = new ProductAdapter();
         productAdapter.setData(category.getProducts());
         holder.rcvProduct_allproducts.setAdapter(productAdapter);
+
+        holder.txtSeeMore_allproducts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentListProduct=new Intent(context, ListProductActivity.class);
+                 context.startActivity(intentListProduct);
+            }
+        });
+
 
     }
 
