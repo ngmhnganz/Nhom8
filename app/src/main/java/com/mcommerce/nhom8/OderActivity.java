1dedcdf31a9298f44bf4ceff767ea6b578d78559
Ngan Nguyen <ngannm19411c@st.uel.edu.vn>
2021-10-29 04:19:42 +0700

    tạo date range pick

--- app/src/main/java/com/mcommerce/nhom8/OderActivity.java
+++ app/src/main/java/com/mcommerce/nhom8/OderActivity.java
@@ -1,8 +1,13 @@
 package com.mcommerce.nhom8;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+
+import com.mcommerce.fragment.HistoryOrderFragment;
 
 public class OderActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class OderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oder);
+
+        linkview();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        HistoryOrderFragment historyOrderFragment = new HistoryOrderFragment();
+        fragmentTransaction.add(R.id.layoutContainerFragment_orderactivity,historyOrderFragment);
+        fragmentTransaction.commit();
+    }
+
+    private void linkview() {
+
     }
 }
