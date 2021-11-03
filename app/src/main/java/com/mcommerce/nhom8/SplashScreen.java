2402b270c3049ac73f450dcf7e8a5b3f97370160
Sunhuynh <xuanhk19411c@st.uel.edu.vn>
2021-11-03 17:53:27 +0700

    Trang Login chưa xử lý được btnLogin

--- app/src/main/java/com/mcommerce/nhom8/SplashScreen.java
+++ app/src/main/java/com/mcommerce/nhom8/SplashScreen.java
@@ -31,7 +31,7 @@ public class SplashScreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent= new Intent(SplashScreen.this,MainActivity.class);
+                Intent intent= new Intent(SplashScreen.this,Login.class);
                 startActivity(intent);
                 finish();
             }
